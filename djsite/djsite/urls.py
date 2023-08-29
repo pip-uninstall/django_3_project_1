f1b40c0c333b25a590f1446e1f0a924688c842dc
pip-uninstall <privalov_nikita@icloud.com>
2023-08-29 20:28:42 +0300

    add tables to db, create view for some pages

--- djsite/djsite/urls.py
+++ djsite/djsite/urls.py
@@ -13,13 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from djsite import settings
 from women.views import *
 from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('women/', include('women.urls')),
+    path('', include('women.urls')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = pageNotFound
--- djsite/women/views.py
+++ djsite/women/views.py
@@ -1,9 +1,28 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseNotFound
+
+from .models import *
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Page Women')
+    posts = Women.objects.all()
+    return render(request, 'women/index.html')
 
-def categories(request):
-    return HttpResponse('Categories')
+def about(request):
+    return render(request, 'women/about.html')
+
+
+def categories(request, catid):
+    if request.POST:
+        return HttpResponse('Categories')
+
+
+def archive(request, year):
+    if int(year) > 2020:
+        return redirect('/', permanent=True)
+
+
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('404')
